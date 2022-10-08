643590f057c849b425b4f35c09c55b505179abd9
aryankuag <alemantrixaryan@gmail.com>
2022-10-08 09:24:54 +0530

    body added

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Main() {
   return (
-    <div className='w-full rounded-full border-8 border-blue-900 p-4 my-4'>Main</div>
-  )
+    <div className="w-full rounded-full border-8 border-blue-900 p-4 my-4 flex flex-col items-center">
+      <p className="text-2xl">Session</p>
+      <p className="text-8xl">00:00</p>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
